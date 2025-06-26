a1b396b133c1b1ed6cd5e15846dca37407a7119a
amir ahmadi <162714606+dukeahmadi@users.noreply.github.com>
2025-06-26 12:42:41 +0330

    server.js

--- server.js
+++ server.js
@@ -1,31 +1,25 @@
 const { create } = require('@open-wa/wa-automate');
-const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const executablePath = browser.executablePath();
-
-  create({
-    sessionId: "waha-render",
-    multiDevice: true,
-    headless: true,
-    qrTimeout: 0,
-    authTimeout: 60,
-    cacheEnabled: false,
-    useChrome: false,
-    killProcessOnBrowserClose: true,
-    disableSpins: true,
-    throwErrorOnTosBlock: false,
-    logConsole: false,
-    popup: false,
-    executablePath,
-    chromiumArgs: ['--no-sandbox', '--disable-setuid-sandbox'],
-    server: true,
-    apiHost: '0.0.0.0',
-    apiPort: process.env.PORT || 8000
-  }).then(client => {
-    console.log('✅ Waha is ready!');
-  }).catch(e => {
-    console.error('❌ Waha failed to start', e);
-  });
-})();
+create({
+  sessionId: "waha-render",
+  multiDevice: true,
+  headless: true,
+  qrTimeout: 0,
+  authTimeout: 60,
+  cacheEnabled: false,
+  useChrome: true,
+  killProcessOnBrowserClose: true,
+  disableSpins: true,
+  throwErrorOnTosBlock: false,
+  logConsole: false,
+  popup: false,
+  chromiumArgs: ['--no-sandbox', '--disable-setuid-sandbox'],
+  executablePath: '/usr/bin/google-chrome', // 🟢 کروم داخلی لینوکس
+  server: true,
+  apiHost: '0.0.0.0',
+  apiPort: process.env.PORT || 8000
+}).then(client => {
+  console.log('✅ Waha is ready!');
+}).catch(e => {
+  console.error('❌ Waha failed to start', e);
+});
