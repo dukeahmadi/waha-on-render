e2d8f4a6cc29136ba5fc91d6a4818e8368b6de10
amir ahmadi <162714606+dukeahmadi@users.noreply.github.com>
2025-06-26 10:47:20 +0330

    .server.js

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
 const { create } = require('@open-wa/wa-automate');
+const puppeteer = require('puppeteer');
 
-create({
-  sessionId: "waha-render",
-  multiDevice: true,
-  headless: true,
-  qrTimeout: 0,
-  authTimeout: 60,
-  cacheEnabled: false,
-  useChrome: true,
-  executablePath: "/usr/bin/chromium-browser", // مسیر مرورگر در سرور Render
-  chromiumArgs: ['--no-sandbox', '--disable-setuid-sandbox'],
-  killProcessOnBrowserClose: true,
-  disableSpins: true,
-  throwErrorOnTosBlock: false,
-  logConsole: false,
-  popup: false,
+(async () => {
+  const browserFetcher = puppeteer.createBrowserFetcher();
+  const revisionInfo = await browserFetcher.download('1221111'); // نسخه پایدار
+  const executablePath = revisionInfo.executablePath;
 
-  // فعال‌سازی API
-  server: true,
-  apiHost: '0.0.0.0',
-  apiPort: process.env.PORT || 8000
-}).then(client => {
-  console.log('✅ Waha is ready!');
-}).catch(e => {
-  console.error('❌ Waha failed to start', e);
-});
+  create({
+    sessionId: "waha-render",
+    multiDevice: true,
+    headless: true,
+    qrTimeout: 0,
+    authTimeout: 60,
+    cacheEnabled: false,
+    useChrome: false,
+    killProcessOnBrowserClose: true,
+    disableSpins: true,
+    throwErrorOnTosBlock: false,
+    logConsole: false,
+    popup: false,
+    executablePath,  // مسیر کروم دانلود شده
+    chromiumArgs: ['--no-sandbox', '--disable-setuid-sandbox'],
+    server: true,
+    apiHost: '0.0.0.0',
+    apiPort: process.env.PORT || 8000
+  }).then(client => {
+    console.log('✅ Waha is ready!');
+  }).catch(e => {
+    console.error('❌ Waha failed to start', e);
+  });
+})();
